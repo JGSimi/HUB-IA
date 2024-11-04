3d07846a0fa47fe76ac77f4239826ce1a49fc241
JGSimi <joao02simi@gmail.com>
2024-11-04 15:13:40 -0300

    Login front

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {/* Aqui você pode adicionar widgets e informações do usuário */}
+        </div>
+    );
+}
+
+export default Dashboard; 
